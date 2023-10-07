bde948df930c5b57f38e68f6716fe1da2a4bf8b7
Jacob Speicher <speicherjacob@gmail.com>
2023-10-07 17:07:11 -0500

    map go away

--- rpg/map.gd
+++ rpg/map.gd
@@ -7,7 +7,11 @@ func _ready():
 	for i in range(max_level):
 		var battle_button = Button.new()
 		battle_button.text = "battle {level}".format({"level": i})
+		battle_button.pressed.connect(_on_button_pressed)
 		%VBoxContainer.add_child(battle_button)
 
 func open_map():
 	current_level += 1
+
+func _on_button_pressed():
+	visible = false
