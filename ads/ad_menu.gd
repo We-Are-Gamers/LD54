5a074f9d19696de1bed8a4de9a100b71a07fcf9f
= <kmtaylor023@gmail.com>
2023-10-07 13:37:49 -0600

    make ad menu a bit more reusable

--- ads/ad_menu.gd
+++ ads/ad_menu.gd
@@ -1,21 +1,36 @@
 extends Control
 
+@export var slow_menu_duration = 0.5
+@export var fast_menu_duration = 0.1
+
 var open = true
 var recent_ad_size
 
-var start_position = Vector2.ZERO
-var end_position = Vector2.ZERO
+var xpos = {}
 
 signal spawn_ad_preview(ad_size, ad_position)
 
-func toggle_menu(toggle, duration = 0.5):
+func _ready():
+	xpos[true] = $MenuPanel.position.x
+	xpos[false] = $MenuPanel.position.x + 250
+	$MenuPanel.position.x = xpos[false]
+	open = false
+	pass
+	
+func open_menu(open_fast = false):
+	if !open:
+		toggle_menu(open_fast)
+		
+func close_menu(open_fast = false):
+	if open:
+		toggle_menu(open_fast)
+
+func toggle_menu(open_fast = false):
+	open = !open
 	var tween = create_tween()
-	if toggle:
-		tween.tween_property($MenuPanel, "position:x", $MenuPanel.position.x - 250, duration)
-	else:
-		tween.tween_property($MenuPanel, "position:x", $MenuPanel.position.x + 250, duration)
+	var duration = fast_menu_duration if open_fast else slow_menu_duration
+	tween.tween_property($MenuPanel, "position:x", xpos[open], duration)
 	tween.play()
-	open = toggle
 
 func show_preview(ad_size):
 	%ad_preview_box.set_ad_size(ad_size)
@@ -36,8 +51,8 @@ func _on_button_mouse_exited():
 
 func _on_button_pressed():
 	spawn_ad_preview.emit(recent_ad_size, get_viewport().get_mouse_position())
-	toggle_menu(false, .1)
+	close_menu(true)
 	hide_preview()
 
 func _on_menu_button_pressed():
-	toggle_menu(!open)
+	toggle_menu()
