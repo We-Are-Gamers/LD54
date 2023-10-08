ac4effdbd7a96ecf0771e7ce4c7ac9a8c0fd3631
= <kmtaylor023@gmail.com>
2023-10-07 23:50:34 -0600

    make add menu buttons a bit better lookin

--- ads/ad_menu_button.gd
+++ ads/ad_menu_button.gd
@@ -1,8 +1,10 @@
-class_name AdDescription extends Button
+class_name AdDescription extends VBoxContainer
 
 var ad_size = Vector2.ZERO : get = _get_ad_size
 
 @export var income_amount: int
+
+@export var button_text: String
 
 @export var income_per_tick: bool
 
@@ -12,12 +14,17 @@ func _get_ad_size():
 	return ad_texture.get_size()
 
 signal preview_ad(ad: AdDescription)
+signal pressed()
 
 func _ready():
-	var size_text = "{x}x{y}".format({"x": ad_size.x, "y": ad_size.y})
+	$Button.text = "{x}x{y}".format({"x": ad_size.x, "y": ad_size.y})
 	var tick_desc = "per tick" if income_per_tick else "when placed"
-	var income_text = "makes ${0} {1}".format([income_amount, tick_desc])
-	text = "{0} {1}".format([size_text, income_text])
+	var income_text = "   makes ${0} {1}".format([income_amount, tick_desc])
+	$Label.text = income_text
 
 func _on_mouse_entered():
 	preview_ad.emit(self)
+
+
+func _on_button_button_down():
+	pressed.emit()
