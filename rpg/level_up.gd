932c868e650e933dff99c9d02d1160a3ab0a3338
Jacob Speicher <speicherjacob@gmail.com>
2023-10-08 14:11:45 -0500

    update level up options

--- rpg/level_up.gd
+++ rpg/level_up.gd
@@ -4,11 +4,16 @@ signal level_up_button(type)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/HBoxContainer/RockButton.pressed.connect(_on_rock_pressed)
-	$VBoxContainer/HBoxContainer/PaperButton.pressed.connect(_on_paper_pressed)
-	$VBoxContainer/HBoxContainer/ScissorsButton.pressed.connect(_on_scissors_pressed)
-	$VBoxContainer/HBoxContainer/HealButton.pressed.connect(_on_heal_pressed)
+	$Control/VBoxContainer/ButtonContainer/RockContainer/RockButton.pressed.connect(_on_rock_pressed)
+	$Control/VBoxContainer/ButtonContainer/PaperContainer/PaperButton.pressed.connect(_on_paper_pressed)
+	$Control/VBoxContainer/ButtonContainer/ScissorsContainer/ScissorsButton.pressed.connect(_on_scissors_pressed)
+	$Control/VBoxContainer/ButtonContainer/HealContainer/HealButton.pressed.connect(_on_heal_pressed)
 
+func set_power_levels(power_levels):
+	$Control/VBoxContainer/ButtonContainer/RockContainer/Label.text = "(Level {lv})".format({"lv": power_levels["rock"]})
+	$Control/VBoxContainer/ButtonContainer/PaperContainer/Label.text = "(Level {lv})".format({"lv": power_levels["paper"]})
+	$Control/VBoxContainer/ButtonContainer/ScissorsContainer/Label.text = "(Level {lv})".format({"lv": power_levels["scissors"]})
+	$Control/VBoxContainer/ButtonContainer/HealContainer/Label.text = "(Level {lv})".format({"lv": power_levels["heal"]})
 
 func reenable():
 	visible = true
