855789e7933ebd0c3b21f94ce8c80d2720acfcac
Jacob Speicher <speicherjacob@gmail.com>
2023-10-08 19:31:59 -0500

    add instructions to main menu

--- main_menu.gd
+++ main_menu.gd
@@ -1,15 +1,29 @@
 extends ColorRect
 
+var AdWindow = preload("res://ads/ad_window.tscn")
+var AdTexture = preload("res://resources/ads/game_instructions.png")
+
 @onready var bank: Banking = get_node("/root/Bank")
 @onready var anim_player = $AnimationPlayer
 
 @export var next_scene_path: String
 
+var menu_ad: AdDescription = AdDescription.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bank.visible = false
+	
+	menu_ad.ad_size = Vector2(300, 150)
+	menu_ad.ad_texture = ImageTexture.create_from_image(AdTexture.get_image())
+	var ad_window = AdWindow.instantiate()
+	ad_window.bank = bank
+	ad_window.position = %Play.position + %Play.texture_normal.get_size() / 2
+	ad_window.set_ad(menu_ad)
+	$"../AdHolder".add_child.call_deferred(ad_window)
+	
 	anim_player.play_backwards("menu_fade")
-	$"../Play".pressed.connect(_on_play_button_pressed)
+	%Play.pressed.connect(_on_play_button_pressed)
 
 
 func scene_transition(next_scene):
